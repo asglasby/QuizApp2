738712c62c19e5fa4afb5fdb9c814ae25c57a5d2
asglasby <aishaextra@gmail.com>
2014-05-09 11:20:49 -0400

    Basic Quiz Function Working

--- ToDoApp2/main.js
+++ ToDoApp2/main.js
@@ -1,80 +1,142 @@
 ﻿"use strict";
 
 //define my question and answer object
+var questionNumber = 0;
 var quizQuestions = {};
-var currentQuestion = ""; //This variable will hold the current question.
+var currentQuestion = "question1"; //This variable will hold the current question.
 //create a property of quizQuestions called question1 which will be an object with 3 properties - question1, answersOptions, and answer.
+var objectCounterArray = [];
+var scoreCounter = 0;
 quizQuestions["question1"] = {
-    question1: "my question",
+    question: "Question 1 -This is one of my questions",
     answerOptions: ["answerOption1", "answerOption2", "answerOption3", "answerOption4"],
     answer: "answerOption1"
 };
 
-//create a function to check the answer
-var checkAnswer1Function = function () {
-    var correctAnswer = quizQuestions[currentQuestion]["answer"];
-    //myClickedAnswer 
-    if (document.getElementById("ans1").innerHTML === quizQuestions[currentQuestion]["answer"]) {
-        alert("Correct");
-    } else {
-        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
-    }
-   // alert("This function is not working yet!");
-}
-
-var checkAnswer2Function = function () {
-    var correctAnswer = quizQuestions[currentQuestion]["answer"];
-    //myClickedAnswer 
-    if (document.getElementById("ans2").innerHTML === quizQuestions[currentQuestion]["answer"]) {
-        alert("Correct");
-    } else {
-        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
-    }
-    // alert("This function is not working yet!");
-}
-
-var checkAnswer3Function = function () {
-    var correctAnswer = quizQuestions[currentQuestion]["answer"];
-    //myClickedAnswer 
-    if (document.getElementById("ans3").innerHTML === quizQuestions[currentQuestion]["answer"]) {
-        alert("Correct");
-    } else {
-        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
-    }
-    // alert("This function is not working yet!");
-}
-
-var checkAnswer4Function = function () {
-    var correctAnswer = quizQuestions[currentQuestion]["answer"];
-    //myClickedAnswer 
-    if (document.getElementById("ans4").innerHTML === quizQuestions[currentQuestion]["answer"]) {
-        alert("Correct");
-    } else {
-        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
-    }
-    // alert("This function is not working yet!");
-}
-
-//create my nextQuestion function
-var goToTheNextQuestion = function () {
-    alert("This button is not working just yet!");
+quizQuestions["question2"] = {
+    question: "Question 2 -This is one of my questions",
+    answerOptions: ["answerOption1", "answerOption2", "answerOption3", "answerOption4"],
+    answer: "answerOption2"
 };
 
-//var myAnswers = "<li id='ans'>" + "myanswers" + "</li>";
+quizQuestions["question3"] = {
+    question: "Question 3 -This is one of my questions",
+    answerOptions: ["answerOption1", "answerOption2", "answerOption3", "answerOption4"],
+    answer: "answerOption3"
+};
 
-for (var i in quizQuestions) {
-    alert(i);
-    //create a function to display the first question
-    currentQuestion = i;
-    var myAnswersFunc = function () {
-        var answers = "<li id='ans1'onclick='checkAnswer1Function();'>" + quizQuestions[i]["answerOptions"][0] + "</li>" +
-                      "<li id='ans2'onclick='checkAnswer2Function();'>" + quizQuestions[i]["answerOptions"][1] + "</li>" +
-                      "<li id='ans3'onclick='checkAnswer3Function();'>" + quizQuestions[i]["answerOptions"][2] + "</li>" +
-                      "<li id='ans4'onclick='checkAnswer4Function();'>" + quizQuestions[i]["answerOptions"][3] + "</li>";
-        return answers;
-    };
-    document.getElementById("question").innerHTML = i +"<br>" + myAnswersFunc();
+quizQuestions["question4"] = {
+    question: "Question 4 -This is one of my questions",
+    answerOptions: ["answerOption1", "answerOption2", "answerOption3", "answerOption4"],
+    answer: "answerOption4"
+};
+
+quizQuestions["question5"] = {
+    question: "Question 5 -This is one of my questions",
+    answerOptions: ["answerOption1", "answerOption2", "answerOption3", "answerOption4"],
+    answer: "answerOption2"
+};
+
+for (var count in quizQuestions) {
+    objectCounterArray.push(count);
+    //alert(objectCounterArray);
 }
+
+//create a function to check the answer
+var checkAnswer1Function = function () {
+    "use strict"
+    var correctAnswer = quizQuestions[currentQuestion]["answer"];
+    if (document.getElementById("ans1").innerHTML === quizQuestions[currentQuestion]["answer"]) {
+        alert("Correct");
+        scoreCounter++;
+    } else {
+        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
+    }
+    document.getElementById("question").innerHTML = "Click Next to Continue";
+};
+
+var checkAnswer2Function = function () {
+    "use strict"
+    var correctAnswer = quizQuestions[currentQuestion]["answer"];
+    if (document.getElementById("ans2").innerHTML === quizQuestions[currentQuestion]["answer"]) {
+        alert("Correct");
+        scoreCounter++;
+    } else {
+        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
+    }
+    document.getElementById("question").innerHTML = "Click Next to Continue";
+
+};
+
+var checkAnswer3Function = function () {
+    "use strict"
+    var correctAnswer = quizQuestions[currentQuestion]["answer"];
+    if (document.getElementById("ans3").innerHTML === quizQuestions[currentQuestion]["answer"]) {
+        alert("Correct");
+        scoreCounter++;
+    } else {
+        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
+    }
+    document.getElementById("question").innerHTML = "Click Next to Continue";
+};
+
+var checkAnswer4Function = function () {
+    "use strict"
+    var correctAnswer = quizQuestions[currentQuestion]["answer"];
+    if (document.getElementById("ans4").innerHTML === quizQuestions[currentQuestion]["answer"]) {
+        alert("Correct");
+        scoreCounter++;
+    } else {
+        alert("Your answer is not correct.  The correct answer is" + correctAnswer);
+    }
+    document.getElementById("question").innerHTML = "Click Next to Continue";
+};
+
+var myAnswersFunc = function () {
+    "use strict";
+    var answers = "<li id='ans1'onclick='checkAnswer1Function();'>" + quizQuestions[currentQuestion]["answerOptions"][0] + "</li>" +
+                  "<li id='ans2'onclick='checkAnswer2Function();'>" + quizQuestions[currentQuestion]["answerOptions"][1] + "</li>" +
+                  "<li id='ans3'onclick='checkAnswer3Function();'>" + quizQuestions[currentQuestion]["answerOptions"][2] + "</li>" +
+                  "<li id='ans4'onclick='checkAnswer4Function();'>" + quizQuestions[currentQuestion]["answerOptions"][3] + "</li>";
+    return answers;
+};
+
+var endQuiz = function () {
+    "use strict";
+    alert("Your Score is " + scoreCounter);
+    document.getElementById("question").innerHTML = "Thank You For Taking The Quiz!";
+    document.getElementById("buttons").innerHTML = ""
+};
+
+//create my nextQuestion function
+//var goToTheNextQuestion = function () {
+//    "use strict";
+//        for (questionNumber = 0; questionNumber < objectCounterArray.length; questionNumber++) {
+//            currentQuestion = objectCounterArray[questionNumber]
+//            document.getElementById("question").innerHTML = quizQuestions[currentQuestion]["question"] + "<br>" + myAnswersFunc();
+//          }
+//        endQuiz();
+//};
+
+var goToTheNextQuestion = function () {
+    "use strict";
+
+    if (questionNumber === objectCounterArray.length - 1) {
+        endQuiz();
+    } else {
+        questionNumber++;
+        currentQuestion = objectCounterArray[questionNumber]
+        document.getElementById("question").innerHTML = quizQuestions[currentQuestion]["question"] + "<br>" + myAnswersFunc();
+    }
+};
+
+document.getElementById("question").innerHTML = quizQuestions[currentQuestion]["question"] + "<br>" + myAnswersFunc();
+
+
+    
+
+
+
 
 
 
